591c47c6d88059dd51efe4a3c6ecb2775979abca
Costa Soutos <costa.soutos@outlook.com>
2023-05-19 09:36:24 -0400

    Added phoneNum attribute

--- src/arraydemos23friam/Student.java
+++ src/arraydemos23friam/Student.java
@@ -6,7 +6,8 @@ public class Student {
     // comment added at 9:27am
     private String sId;
     private String sName;
-
+    private String phoneNum;
+    
     public Student(String sId, String sName) {
         this.sId = sId;
         this.sName = sName;
